ad1eb06b31db8c82357184654de84aa1417a40fb
agustin.perez <perezp.agustin@gmail.com>
2020-11-21 03:50:18 -0300

    Integrated signup component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { Signup } from './components'
+import { Container } from 'react-bootstrap' 
 
 function App() {
   return (
-    <h1>Hello World</h1>
+		<Container 
+			className='d-flex align-items-center justify-content-center'
+			style={{ minHeight: '100vh' }}>
+			<div 
+				className='w-100'
+				style={{ maxWidth: '400px' }}>
+					<Signup />
+			</div>
+		</Container>
   )
 }
 
